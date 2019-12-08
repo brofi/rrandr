c127a23d31a92e7c4df3d681644456bd18589508
Andy B <brofi.archlinux@gmail.com>
2019-12-08 06:35:38 +0100

    print connected outputs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::ffi::CStr;
+use std::ptr::null;
+
+use x11::xlib::{Display, Window, XCloseDisplay, XDefaultScreen, XOpenDisplay, XRootWindow};
+use x11::xrandr::{RR_Connected, RROutput, XRRGetOutputInfo, XRRGetScreenResourcesCurrent, XRROutputInfo, XRRScreenResources};
+
 fn main() {
-    println!("Hello, world!");
+    print_connected_outputs();
+}
+
+fn print_connected_outputs() {
+    unsafe {
+        let dpy: *mut Display = XOpenDisplay(null());
+
+        if dpy.is_null() {
+            panic!("Failed to open display.");
+        }
+
+        let screen = XDefaultScreen(dpy);
+        let root: Window = XRootWindow(dpy, screen);
+        let res: *mut XRRScreenResources = XRRGetScreenResourcesCurrent(dpy, root);
+
+        for o in 0..(*res).noutput {
+            let output: RROutput = *(*res).outputs.offset(o as isize);
+            let output_info: *mut XRROutputInfo = XRRGetOutputInfo(dpy, res, output);
+            let name = CStr::from_ptr((*output_info).name).to_str().unwrap();
+
+            #[allow(non_upper_case_globals)]
+                match (*output_info).connection as i32 {
+                RR_Connected => println!("{}", &name),
+                _ => ()
+            }
+        }
+
+        XCloseDisplay(dpy);
+    }
 }
