2f67d327bebc6fc10042cff7a8578da89b3bb923
Andy B <brofi.archlinux@gmail.com>
2019-12-08 21:50:15 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 extern crate gio;
 extern crate gtk;
 
-use std::env;
 use std::ffi::CStr;
 use std::ptr::null;
+use std::{env, slice};
 
 use gio::prelude::*;
 use gtk::prelude::*;
@@ -59,28 +59,20 @@ fn print_connected_outputs() {
             panic!("Failed to open display.");
         }
 
-        let screen = XDefaultScreen(dpy);
-        let root: Window = XRootWindow(dpy, screen);
-        let res: *mut XRRScreenResources = XRRGetScreenResourcesCurrent(dpy, root);
+        let res: *mut XRRScreenResources = get_screen_res(&mut *dpy);
+        let outputs: Vec<RROutput> = get_outputs_as_vec(&mut *res);
 
-        for o in 0..(*res).noutput {
-            let output: RROutput = *(*res).outputs.offset(o as isize);
-            let output_info: *mut XRROutputInfo = XRRGetOutputInfo(dpy, res, output);
-
+        for o in outputs {
+            let output_info: *mut XRROutputInfo = XRRGetOutputInfo(dpy, res, o);
             if is_connected((*output_info).connection) {
                 let name = CStr::from_ptr((*output_info).name).to_str().unwrap();
                 println!("{}", &name);
 
-                println!("modes:");
-                for om in 0..(*output_info).nmode {
-                    let mode: RRMode = *(*output_info).modes.offset(om as isize);
-                    for rm in 0..(*res).nmode {
-                        let mode_info: XRRModeInfo = *(*res).modes.offset(rm as isize);
-                        if mode_info.id == mode {
-                            let mode_name = CStr::from_ptr(mode_info.name).to_str().unwrap();
-                            println!("{} ({:.2} Hz)", &mode_name, get_refresh_rate(mode_info));
-                        }
-                    }
+                let mode_info: Vec<XRRModeInfo> =
+                    get_mode_info_for_output_as_vec(&mut *res, &mut *output_info);
+                for mode_i in mode_info {
+                    let mode_name = CStr::from_ptr(mode_i.name).to_str().unwrap();
+                    println!("{} ({:.2} Hz)", &mode_name, get_refresh_rate(mode_i));
                 }
             }
         }
@@ -89,12 +81,63 @@ fn print_connected_outputs() {
     }
 }
 
+fn get_screen_res(dpy: &mut Display) -> *mut XRRScreenResources {
+    unsafe {
+        let screen = XDefaultScreen(dpy);
+        let root: Window = XRootWindow(dpy, screen);
+        let res: *mut XRRScreenResources = XRRGetScreenResourcesCurrent(dpy, root);
+        assert!(!res.is_null());
+        res
+    }
+}
+
+fn get_outputs_as_vec(res: &mut XRRScreenResources) -> Vec<RROutput> {
+    let outputs: *mut RROutput = res.outputs;
+    let len = res.noutput;
+    assert!(!outputs.is_null());
+    assert!(len >= 0);
+    unsafe { slice::from_raw_parts(outputs, len as usize) }.to_vec()
+}
+
+fn get_mode_info_as_vec(res: &mut XRRScreenResources) -> Vec<XRRModeInfo> {
+    let mode_info: *mut XRRModeInfo = res.modes;
+    let len = res.nmode;
+    assert!(!mode_info.is_null());
+    assert!(len >= 0);
+    unsafe { slice::from_raw_parts(mode_info, len as usize) }.to_vec()
+}
+
+fn get_modes_as_vec(output_info: &mut XRROutputInfo) -> Vec<RRMode> {
+    let modes: *mut RRMode = output_info.modes;
+    let len = output_info.nmode;
+    assert!(!modes.is_null());
+    assert!(len >= 0);
+    unsafe { slice::from_raw_parts(modes, len as usize) }.to_vec()
+}
+
+fn get_mode_info_for_output_as_vec(
+    res: &mut XRRScreenResources,
+    output_info: &mut XRROutputInfo,
+) -> Vec<XRRModeInfo> {
+    let mut mode_info_for_output: Vec<XRRModeInfo> = Vec::new();
+    let mode_ids_for_output: Vec<RRMode> = get_modes_as_vec(output_info);
+    let mode_info: Vec<XRRModeInfo> = get_mode_info_as_vec(res);
+    for mode_id in mode_ids_for_output {
+        for mode_i in &mode_info {
+            if mode_id == (*mode_i).id {
+                mode_info_for_output.push(*mode_i);
+            }
+        }
+    }
+    mode_info_for_output
+}
+
 fn is_connected(connection: Connection) -> bool {
     #[allow(non_upper_case_globals)]
-    return match connection as i32 {
+    match connection as i32 {
         RR_Connected => true,
         _ => false,
-    };
+    }
 }
 
 fn get_refresh_rate(mode_info: XRRModeInfo) -> f64 {
@@ -107,9 +150,9 @@ fn get_refresh_rate(mode_info: XRRModeInfo) -> f64 {
         v_total /= 2;
     }
 
-    return if mode_info.hTotal > 0 && v_total > 0 {
+    if mode_info.hTotal > 0 && v_total > 0 {
         mode_info.dotClock as f64 / (mode_info.hTotal as f64 * v_total as f64)
     } else {
         0.0
-    };
+    }
 }
